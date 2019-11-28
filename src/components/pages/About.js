5dcc9419e96e55c00567bf49552f458cb9e977c3
TGrooves-208 <tgrooves.208@gmail.com>
2019-11-28 09:49:39 -0700

    Initial commit of a contact manager react application

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">
+        This is a react app using the context api with jsonplaceholder to mimic
+        the backend.
+      </p>
+      <p className="text-secondary">Version 1.0.0</p>
+    </div>
+  );
+};
